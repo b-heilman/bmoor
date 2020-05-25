be53d013da3a1366a430c0fde971dda882103bcb
Brian Heilman <brian.heilman@gmail.com>
2020-05-25 07:37:00 -0400

    Update error.js
    making error functional and removing references to this

--- src/lib/error.js
+++ src/lib/error.js
@@ -2,56 +2,72 @@
 const uuid = require('uuid/v1');
 const {levels} = require('./logger.js');
 
-module.exports = {
-	addStatus(err, ctx =  {}){
-		if (!err.ref){
-			err.ref = uuid();
-		}
+function addStatus(err, ctx =  {}){
+	if (!err.ref){
+		err.ref = uuid();
+	}
 
-		if (ctx.status){
-			err.status = ctx.status;
-			err.type = ctx.type || levels.error;
-		}
+	if (ctx.status){
+		err.status = ctx.status;
+		err.type = ctx.type || levels.error;
+	}
 
-		return err;
-	},
+	return err;
+}
 
-	addTrace(err, ctx = {}){
-		const context = ctx.context || {};
+function addTrace(err, ctx = {}){
+	const context = ctx.context || {};
 
-		if (ctx.code){
-			if (err.code){
-				if (!err.trace){
-					err.trace = [];
-				}
-
-				err.trace.unshift({
-					code: err.code,
-					context: err.context
-				});
+	if (ctx.code){
+		if (err.code){
+			if (!err.trace){
+				err.trace = [];
 			}
 
-			err.code = ctx.code;
-			err.context = context;
+			err.trace.unshift({
+				code: err.code,
+				context: err.context
+			});
 		}
 
-		return err;
-	},
-
-	addResponse(err, ctx){
-		if (ctx.response){
-			err.response = ctx.response;
-			err.payload = ctx.payload || {};
-		}
-
-		return err;
-	},
-
-	apply(err, ctx){
-		this.addStatus(err, ctx);
-		
-		this.addTrace(err, ctx);
-
-		return this.addResponse(err, ctx);
+		err.code = ctx.code;
+		err.context = context;
 	}
+
+	return err;
+}
+
+function addResponse(err, ctx){
+	if (ctx.response){
+		err.response = ctx.response;
+		err.payload = ctx.payload || {};
+	}
+
+	return err;
+}
+
+function apply(err, ctx){
+	addStatus(err, ctx);
+	
+	addTrace(err, ctx);
+
+	return addResponse(err, ctx);
+}
+
+function create(msg, ctx){
+	const err = new Error(msg);
+
+	return apply(err, ctx);
+}
+
+module.exports = {
+	addStatus,
+
+	addTrace,
+
+	addResponse,
+
+	apply,
+
+	create
 };
