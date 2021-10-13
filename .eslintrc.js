4bdc9943436fafe362d456e77ae262591a6fea1c
Brian Heilman <brian.heilman@gmail.com>
2021-10-12 20:28:56 -0400

    add: typescript... sorta

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,25 @@ module.exports = {
         "es2021": true,
         "mocha": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        // "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "ecmaVersion": 13
+        "ecmaVersion": 13,
+        "sourceType": "module"
     },
+    "plugins": ["@typescript-eslint"],
     "rules": {
-    }
+    },
+    "overrides": [{
+        "files": "**/*.ts",
+        "extends": [
+            "eslint:recommended",
+            "plugin:@typescript-eslint/recommended",
+            "plugin:prettier/recommended"
+        ]
+    }]
 };
